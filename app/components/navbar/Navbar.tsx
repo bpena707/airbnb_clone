db0abeb46e2871d43162eb37c46c07f1338d1012
bpena707 <bpena.610@gmail.com>
2023-04-02 19:03:57 -0700

    set up
    set up next js and tailwind configurations.
    
    created container that holds children which is used in the navbar

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+
+import Container from '../Container'
+
+function Navbar() {
+  return (
+    <div className='fixed w-full bg-white z-10 shadow-sm'>
+        <Container>
+            anything
+        </Container>
+    </div>
+  )
+}
+
+export default Navbar
