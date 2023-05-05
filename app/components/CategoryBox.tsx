52f13f210f536742faa46cb74db6a300ee851806
bpena707 <bpena.610@gmail.com>
2023-05-05 15:48:21 -0700

    query
    added the querying capability for when the box is clicked it will route to the new url that is created.

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -3,7 +3,10 @@ the category box is where the icon along with the rest of the attributes will di
 */
 'use client'
 
+import { useRouter, useSearchParams } from "next/navigation"
+import { useCallback } from "react"
 import { IconType } from "react-icons"
+import qs from "query-string";
 
 
 interface CategoryBoxProps {
@@ -13,10 +16,43 @@ interface CategoryBoxProps {
 }
 
 const CategoryBox: React.FC<CategoryBoxProps> = ({
-    icon: Icon,
+    icon: Icon,//alias is given so that it can be used as a comoponent
     label,
     selected
 }) => {
+    const router = useRouter()
+    const params = useSearchParams()
+
+    //define the empty query 
+    const handleClick = useCallback(() => {
+        let currentQuery = {}
+
+        //check to see if we have params at all 
+        //create an object out of all current parameters by parsing 
+        if (params) {
+            currentQuery= qs.parse(params.toString())
+        }
+
+        //when clicking on the category boxes the current label is assigned as the category parent in url
+        //spread query and add new category
+        const updatedQuery: any = {
+            ...currentQuery,
+            category: label
+        }
+
+        //if we already selected a category we want it to remove all categoires (resets category when selected)
+        if (params?.get('category') === label) {
+            delete updatedQuery.category
+        }
+
+        //generate url with the newest query 
+        const url = qs.stringifyUrl({
+            url: '/',
+            query: updatedQuery
+        }, {skipNull: true})//filter out empty options
+
+        router.push(url)
+    },[label, params, router])
   return (
     <div className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 cursor-pointer ${selected ? 'border-b-neutral-800' : 'border-transparent'} ${selected ? 'text-neutral-800' : 'text-neutral-500'}`}>
         <Icon size={26} />
