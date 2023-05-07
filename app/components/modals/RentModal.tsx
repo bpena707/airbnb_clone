14008d0a957aef6d96e9e738c148334bbf2157bf
bpena707 <bpena.610@gmail.com>
2023-05-06 18:54:31 -0700

    steps
    creted the steps that the user will go through in order to airbnb their home the fuctionality for the button was added so that the user can navigate through the steps and edge cases were taken care of to make sure user stays wihtin boundary of steps

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,13 +1,56 @@
 /* the rent modal pops up when clicking on the airbnb your home button */
 
 'use client'
+import { useMemo, useState } from 'react';
 import Modal from './Modal'
 import useRentModal from "@/app/hooks/useRentModal";
 
+//set of named constants 
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5
+}
+
+// control for the steps
 
 
 const RentModal = () => {
     const rentModal = useRentModal()
+
+    const [step, setStep] = useState(STEPS.CATEGORY)
+
+    // functions that go backward and forward
+    function onBack()  {
+        setStep((prevState) => prevState + 1 )
+    }
+
+    function onNext() {
+        setStep((prevState) => prevState - 1)
+    } 
+
+    //if the action label tries to go beyond the price step it triggers the create and then next
+    const actionLabel = useMemo(() => {
+        if(step === STEPS.PRICE) {
+            return 'Create'
+        }
+
+        return 'Next'
+    }, [step])
+
+    // if the secondary action label tries to go back the step is returned as undefined and the returns back
+    const secondaryActionLabel = useMemo(() => {
+        if(step === STEPS.CATEGORY) {
+            return undefined
+        }
+
+        return 'Back'
+    }, [step])
+
+    //body content is dynamic and based on what step we are on
 
   return (
     <div>
@@ -15,7 +58,9 @@ const RentModal = () => {
             onClose={rentModal.onClose}
             isOpen={rentModal.isOpen}
             onSubmit={rentModal.onClose}
-            actionLabel='Submit'
+            actionLabel={actionLabel}
+            secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack} //chekc if we are on the first step and undefined if doesnt exist
             title='Airbnb your home!'
         />
     </div>
