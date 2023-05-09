8b38c6a3c1eafba81933d9cf8c00c60c248de6f1
bpena707 <bpena.610@gmail.com>
2023-05-08 20:56:43 -0700

    country selector
    implemented the country select drop down bar which displays country names and flags along with classnames styling to adhere to general styling from the app.

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -29,7 +29,31 @@ const CountrySelect: React.FC<CountrySelectProps> = ({
             placeholder='Anywhere'
             isClearable
             options={getAll()} //all options displayed using the getAll from use countries hook 
-            onChange={(value) => onChange(value as CountrySelectValue)}
+            onChange={(value) => onChange(value as CountrySelectValue)} //call the prop on change and calls the value as country select
+            formatOptionLabel={(option: any) => (
+                <div className='flex flex-row items-center gap-3'>
+                    <div>{option.flag}</div>
+                    <div>
+                        {option.label}, <span className='text-neutral-500 ml-1'>{option.region}</span>
+                    </div>
+
+                </div>
+            )}
+            //these are the classnames that format thb select box to match the theme 
+            classNames={{
+                control: () =>'p-3 border-2', //thickness of border 
+                input: () => 'text-lg', 
+                option: () =>'text-lg' //text within the select 
+            }}
+            theme={(theme) => ({
+                ...theme,
+                borderRadius: 6, //border radius of the bar 
+                colors: {
+                    ...theme.colors,
+                    primary: 'black', //this the color of the border
+                    primary25: '#ffe4e6' //this is the highlighted bar in select
+                }
+            })}
         />
     </div>
   )
