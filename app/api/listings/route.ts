4a19f477788a85d9561cf775c2d4bbd919e67fc7
bpena707 <bpena.610@gmail.com>
2023-05-17 14:02:06 -0700

    update route.ts
    included all of the json data that is passed for the database

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -2,6 +2,43 @@ import prisma from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
-export async function Post() {
-    
+export async function Post(
+    req: Request
+) {
+    const currentUser = await getCurrentUser()
+
+    if (!currentUser) {
+        return NextResponse.error()
+    }
+
+    const body = await req.json()
+    //extract all from the json body
+    const{
+        title,
+        description,
+        imageSrc,
+        category,
+        roomCount,
+        bathroomCount,
+        guestCount,
+        location,
+        price,
+    } = body
+
+    const listing = await prisma.listing.create({
+        data: {
+            title,
+            description,
+            imageSrc,
+            category,
+            roomCount,
+            bathroomCount,
+            guestCount,
+            locationValue: location.value,
+            price: parseInt(price, 10),
+            userId: currentUser.id
+        }
+    })
+
+    return NextResponse.json(listing)
 }
