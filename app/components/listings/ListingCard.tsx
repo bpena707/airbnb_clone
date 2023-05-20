45ec1b13a3a6a3d0cd68ccddd84e46648a2a6c35
bpena707 <bpena.610@gmail.com>
2023-05-20 11:45:38 -0700

    update listingCard
    created the logic for setting the price and the date and styled the cards that are displayed on the home page

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -4,7 +4,9 @@ import useCountries from "@/app/hooks/UseCountries"
 import { SafeUser } from "@/app/types"
 import { Listing, Reservation } from "@prisma/client"
 import { useRouter } from "next/navigation"
-import { useCallback } from "react"
+import { useCallback, useMemo } from "react"
+import { format } from "date-fns"
+import Image from "next/image"
 
 interface ListingCardProps {
     data: Listing
@@ -44,10 +46,50 @@ const ListingCard: React.FC<ListingCardProps> = ({
             onAction?.(actionId)
         },[disabled, onAction, actionId])
         
-        
+        // price data is memoized and depends on changes to reservationa nd price if reservation exists the total price is displayed
+    const price = useMemo(() => {
+        if (reservation) {
+            return reservation.totalPrice
+        }
+
+        return data.price
+    },[reservation, data.price])
+
+    // constant for the reservation date displyas from, start and end date
+    const reservationDate = useMemo(() => {
+        if (!reservation) {
+            return null
+        }
+
+        const start = new Date(reservation.startDate)
+        const end = new Date(reservation.endDate)
+
+        return `${format(start, 'PP')} - ${format(end, 'PP')}`
+
+
+    },[reservation])
+
 
   return (
-    <div></div>
+    // this div is for the back the card image sits on 
+    <div 
+        onClick={() => router.push(`/listing/${data.id}`)} //this router will push to the single listing when user clicks on it
+        className="col-span-1 cursor-pointer group "
+    >
+        {/* this div formats the order of the cards  */}
+        <div className="flex flex-col gap-2 w-full">
+        {/* this is the image that is placed on the the card div */}
+            <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+                <Image 
+                    fill
+                    alt="Listing"
+                    src={data.imageSrc}
+                    className="object-cover h-full w-full group-hover:scale-110 transition"
+                />
+
+            </div>
+        </div>
+    </div>
   )
 }
 
