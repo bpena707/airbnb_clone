a89747c17cf1179a9fdc3df36a3e6e7110666f53
bpena707 <bpena.610@gmail.com>
2023-05-25 14:17:41 -0700

    date info
    created the reservations part including the functions to create a range of dates.

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -8,6 +8,16 @@ import { Reservation } from "@prisma/client"
 import { useMemo } from "react"
 import ListingHead from "../../components/listings/ListingHead"
 import ListingInfo from "@/app/components/listings/ListingInfo"
+import useLoginModal from "@/app/hooks/useLoginModal"
+import { useRouter } from "next/navigation"
+import { eachDayOfInterval } from "date-fns"
+
+// this constant sets the start and end date 
+const initialDateRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+}
 
 interface ListingClientProps {
     reservations?: Reservation[]
@@ -19,8 +29,32 @@ interface ListingClientProps {
 
 const ListingClient: React.FC<ListingClientProps> = ({
     listing,
+    reservations = [], // reservations is set to an empty array in case there are no reservations
     currentUser
 }) => {
+    const loginModal = useLoginModal()
+    const router = useRouter()
+
+    // iterate over reservations and see any dates we have to disable for reservation in case there is a reservation already booked for it
+    const disabledDates = useMemo(() => {
+        let dates: Date[] = [] //this is an empty array of dates
+
+        // this is the part where all of the reservations are iterated over. a range of dates is created between start and end date
+        reservations.forEach((reservation: any) => {
+            const range = eachDayOfInterval({
+                start: new Date(reservation.startDate),
+                end: new Date(reservation.endDate)
+            })
+
+            // dates are passed to already existing dates array
+            dates = [...dates, ...range]
+        })
+
+        return dates
+    },[reservations])
+
+    // TODO: states and functions to create reservations
+
     const category = useMemo(() => {
         return categories.find((item) => 
         item.label === listing.category)
